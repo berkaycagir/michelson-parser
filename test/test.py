8d18eeefd0733b599f0981223b0f32b4f14d00ab
Hasan Berkay Çağır <berkay@cagir.com.tr>
2021-11-29 15:09:19 +0100

    Small test script update

--- test/test.py
+++ test/test.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 
-import os, subprocess, ast
+import os, subprocess, ast, sys
+
+exit_code = []
 
 for file in os.listdir("test"):
     print("• Testing file {}... ".format(file), end="")
     content = subprocess.run(['cat', "test/" + file], check=True, capture_output=True)
     execution = subprocess.run(["nearley-test", "grammar_l.js", "-q"], input=content.stdout, capture_output=True)
+    exit_code.append(execution.returncode)
     if execution.returncode != 0:
         print("execution resulted in error. ✖ stderr below:")
         print(execution.stderr.decode("utf-8"))
@@ -16,3 +19,9 @@ for file in os.listdir("test"):
         print("all good. ✔")
     else:
         print("unknown error with this file. ✖")
+    print()
+
+if all(v == 0 for v in exit_code):
+    sys.exit()
+else:
+    sys.exit(1)
