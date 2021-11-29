1f032bdc0b633deacc22f5576787ff3c56a994f1
Hasan Berkay Çağır <berkay@cagir.com.tr>
2021-11-29 15:15:51 +0100

    Small test script update v2

--- test/test.py
+++ test/test.py
@@ -2,26 +2,30 @@
 
 import os, subprocess, ast, sys
 
-exit_code = []
+errors = []
 
 for file in os.listdir("test"):
     print("• Testing file {}... ".format(file), end="")
     content = subprocess.run(['cat', "test/" + file], check=True, capture_output=True)
     execution = subprocess.run(["nearley-test", "grammar_l.js", "-q"], input=content.stdout, capture_output=True)
-    exit_code.append(execution.returncode)
     if execution.returncode != 0:
+        errors.append(file)
         print("execution resulted in error. ✖ stderr below:")
         print(execution.stderr.decode("utf-8"))
     elif len(ast.literal_eval(execution.stdout.decode("utf-8"))) == 0 or len(ast.literal_eval(execution.stdout.decode("utf-8"))) > 1:
+        errors.append(file)
         print("execution resulted in multiple parsings. ✖ Results below:")
         print(execution.stdout.decode("utf-8"))
     elif len(ast.literal_eval(execution.stdout.decode("utf-8"))) == 1:
         print("all good. ✔")
     else:
+        errors.append(file)
         print("unknown error with this file. ✖")
     print()
 
-if all(v == 0 for v in exit_code):
-    sys.exit()
-else:
+if len(errors) > 0:
+    print("Problematic files:")
+    print(errors)
     sys.exit(1)
+else:
+    sys.exit()
